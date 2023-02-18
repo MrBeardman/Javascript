0e0d3db5b25d07bd0027a513062b3d26c5652e2b
John Matyas <22-7517-1@plus4u.net>
2023-02-18 20:32:37 +0100

    React air-bnb-experience-clone and travel-journal mini projects.

--- React/ReactCourse/air-bnb-experience-clone/src/App.js
+++ React/ReactCourse/air-bnb-experience-clone/src/App.js
@@ -1,16 +1,26 @@
-import './App.css';
-import Navbar from './components/navbar';
-import Hero from './components/hero';
-import Card from './components/card';
+import "./App.css";
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import Card from "./components/card";
+import data from "./components/data";
 
 function App() {
+  let cards = data.map((item) => (
+    <Card
+      key = {data.id}
+      item ={item}
+    />
+  ));
   return (
     <div>
-    <Navbar/>
-    <Hero/>
-    <Card/>
+      <Navbar />
+      <Hero />
+      <section className="cards-list">
+      {cards}
+      </section>
+
     </div>
-   )
+  );
 }
 
 export default App;
--- React/ReactCourse/air-bnb-experience-clone/src/components/card.js
+++ React/ReactCourse/air-bnb-experience-clone/src/components/card.js
@@ -1,20 +1,28 @@
 import React from "react";
-import cardImg from "../image 12.png";
 import icon from "../Star 1.png";
-export default function Card() {
+
+
+export default function Card(props) {
+  let badgeText
+  console.log(badgeText)
+  if (props.item.openSpots === 0){
+    badgeText =" SOLD OUT"
+  }else if(props.location === "Online"){
+    badgeText ="ONLINE"
+  }
   return (
     <div className="cardRow">
       <div className="card">
-        <img src={cardImg} alt="swimmer" className="card--img"></img>
-        <p className="card--status">Sold out</p>
+        <img src={props.item.coverImg} alt="swimmer" className="card--img"></img>
+        {badgeText && <p className="card--status">{badgeText}</p>}
         <span className="card--span">
           <img src={icon} alt="star icon"></img>
-          <span>5.0 </span>
-          <span className="card--span--gray">(6) • USA</span>
+          <span>{props.item.stats.rating} </span>
+          <span className="card--span--gray">({props.item.stats.reviewCount}) • {props.item.location}</span>
         </span>
-        <p className="card--p">Life lessons with Katie Zaferes</p>
+        <p className="card--p">{props.item.title}</p>
         <span className="span--bold"> 
-          From 136$ <span>/ person</span>
+          From {props.item.price}$ <span>/ person</span>
         </span>
       </div>
     </div>
