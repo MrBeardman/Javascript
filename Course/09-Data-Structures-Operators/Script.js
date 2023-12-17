e2a2973c8da13eabda97d852a9bcd41a6dbf4d87
Jan Matyáš <honza.matyas@gmail.com>
2023-12-17 11:03:55 +0100

    Challenge 3 update

--- Course/09-Data-Structures-Operators/Script.js
+++ Course/09-Data-Structures-Operators/Script.js
@@ -200,7 +200,7 @@ for (const [key, { open, close }] of entries) {
 }
 
 //CODING CHALLENGE 2
-/* 
+/*
 Let's continue with our football betting app!
 
 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
@@ -386,18 +386,66 @@ const question = new Map([
 //SAME structure as calling Object.entries()
 console.log(question.get("question"));
 const answer = 3;
-//const answer = Number(prompt("Your answer"));
-// let response = "";
-// for (const [key, value] of question) {
-//   if (typeof key === "number") {
-//     console.log(`Answer ${key}: ${value}`);
-//   }
-//   if ((response = question.get("correct") === answer)) {
-//     console.log(`Answer ${key}: ${value} is ${question.get(response)}`);
-//   }
-// }
+// const answer = Number(prompt("Your answer"));
+let response = "";
+for (const [key, value] of question) {
+  if (typeof key === "number") {
+    console.log(`Answer ${key}: ${value}`);
+  }
+  if ((response = question.get("correct") === answer)) {
+    console.log(`Answer ${key}: ${value} is ${question.get(response)}`);
+  }
+}
 
 //Convert Map to an array
 console.log([...question]);
 
-//Which data to use
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+  [17, "⚽️ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽️ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽️ GOAL"],
+  [80, "⚽️ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+//1. Create an array 'events' of the different game events that happened (no duplicates)
+const events = new Set([...gameEvents.values()]);
+console.log(events);
+//2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+gameEvents.delete(64);
+//3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+const minutes = [...gameEvents.keys()];
+console.log(minutes);
+
+const lastMinute = minutes.pop();
+console.log(lastMinute);
+let average = lastMinute / minutes.length;
+console.log(`An event happened on average every ${average} minutes`);
+//4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+//[FIRST HALF] 17: ⚽️ GOAL
+
+for (const [minute, event] of gameEvents) {
+  console.log(
+    `${minute < 45 ? "[FIRST GALF]" : "[SECOND HALF]"} ${minute}: ${event}`
+  );
+}
