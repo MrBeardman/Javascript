641650704ad71b48ed6af8bd676a3fb8442132a5
John Matyas <22-7517-1@plus4u.net>
2024-01-31 23:04:54 +0100

    update with slice method

--- Course/11-Arrays-Bankist/script.js
+++ Course/11-Arrays-Bankist/script.js
@@ -74,3 +74,14 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+//Slice(start index, end index ) returns a new array, negative start index start form the end
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2, 4));
+console.log(arr.slice(-2, 4));
+console.log(arr.slice(-2, -1));
+console.log([...arr]);
+arr.splice(-1);
+console.log(arr);
