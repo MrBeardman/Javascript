74a8858544dcc8ec6d30082901815514627df20e
Jan Matyáš <honza.matyas@gmail.com>
2024-01-31 17:13:45 +0100

    Update 31.01.2024 finished the section

--- Course/10-Functions/script.js
+++ Course/10-Functions/script.js
@@ -144,26 +144,130 @@ const poll = {
   options: ["0: Javascript", "1: Python", "2: Rust", "3: C++"],
   answers: new Array(4).fill(0),
   registerNewAnswer() {
-    if (promptOptions.length <= 0) {
-      for (let index = 0; index < poll.options.length; index++) {
-        promptOptions = promptOptions + poll.options[index] + `\n`;
-      }
-    }
-    let answer = Number(prompt(`${poll.question} \n${promptOptions}`));
+    let answer = Number(
+      prompt(
+        `${this.question} \n${this.options.join("\n")} \n(Write otpion numer)`
+      )
+    );
     console.log(answer);
     if (isNaN(answer)) {
       alert("This is not a number");
     } else if (answer >= 0 && answer < this.options.length) {
       poll.answers[answer]++;
-      console.log(poll.answers);
+      console.log(this.answers);
     } else {
       ("");
       alert("Number is too big");
     }
+    this.displayResults();
+    this.displayResults("string");
+  },
+  displayResults(type = "array") {
+    if (type === "array") {
+      console.log(this.answers);
+    } else if (type === "string") {
+      console.log(`Poll resutls are: ${this.answers.join(", ")}`);
+    }
   },
 };
 
-document
-  .querySelector(".poll")
-  .addEventListener("click", poll.registerNewAnswer.bind(poll));
-console.log(poll);
+// document
+//   .querySelector(".poll")
+//   .addEventListener("click", poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, "string");
+
+// [5, 2, 3]
+// [1, 5, 3, 9, 6, 1]
+
+//*IMIDIETLY INVOKED FUNCTION EXPRESSIONS(IIFE)
+
+const runOnce = function () {
+  console.log("Tis will run once");
+};
+runOnce();
+//IIFE
+(function () {
+  console.log("Tis will run once");
+})();
+
+//Works for arrow fucnions
+(() => console.log("Tis will run once"))();
+
+{
+  const isPriave = 23;
+  var notPrivate = "Hey Pussy";
+}
+//console.log(isPriave); //?Variable is not defined
+console.log(notPrivate);
+
+//*CLOSURES
+
+const secureBooking = function () {
+  let passageCount = 0;
+  return function () {
+    passageCount++;
+    console.log(`${passageCount} passengers`);
+  };
+};
+const booker = secureBooking();
+booker();
+booker();
+booker();
+
+//Example 1
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+g(); //nothing
+f(); //46 can access the "a" variable
+
+//Re-assigning the f fucnion
+h(); //nothing
+f(); // 1554
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+  setTimeout(function () {
+    console.log(`we are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+  console.log(`will start boarding in ${wait} seconds`);
+};
+const perGroup = 1000; // will be used if the perGroup in the fucntion woudl not exist
+boardPassengers(180, 3);
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+  document.body.addEventListener("click", function () {
+    header.style.color = "blue";
+  });
+})();
