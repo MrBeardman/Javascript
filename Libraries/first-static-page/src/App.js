8d8ff9f5b0dd15622ee7460da6a6faf07d1bf21d
Jan Matyáš <50779169+MrBeardman@users.noreply.github.com>
2024-03-10 19:07:26 +0100

    added all files

---
+++ Libraries/first-static-page/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
